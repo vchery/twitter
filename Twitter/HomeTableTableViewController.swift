fc047d97d3be684468fbada0caee72e5c5442ff9
vchery <valerie78@ufl.edu>
2021-09-24 13:32:56 -0400

    updated xcode file

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Valerie Chery on 9/24/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
